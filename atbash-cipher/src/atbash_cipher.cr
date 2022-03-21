f048df4be6881be1ecab4cc2f3a3519cb66f0d5a
Andrew Knoblach <AMKnoblach@gmail.com>
2022-03-21 14:07:15 -0700

    Testing precommit hook v2

--- atbash-cipher/src/atbash_cipher.cr
+++ atbash-cipher/src/atbash_cipher.cr
@@ -1,9 +1,11 @@
 module AtbashCipher
   extend self
   ALP = ('a'..'z').join
+
   def encode(s)
     decode(s).scan(/.{1,5}/).map(&.[0]).join(" ")
   end
+
   def decode(s)
     s.downcase.gsub(/[^a-z0-9]/, "").tr(ALP, ALP.reverse)
   end
