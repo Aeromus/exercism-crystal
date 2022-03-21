9715282907aec84770aaca1d460c32b551b95fdf
Andrew Knoblach <AMKnoblach@gmail.com>
2022-03-21 14:30:32 -0700

    Testing precommit hook v3

--- acronym/src/acronym.cr
+++ acronym/src/acronym.cr
@@ -1,8 +1,8 @@
 class Acronym
-  def self.abbreviate(words)
-    words.scan(/[a-zA-Z']+/).map(&.[0][0]).join.upcase
+        def self.abbreviate(words)
+words.scan(/[a-zA-Z']+/).map(&.[0][0]).join.upcase
   end
-end
+          end
 
 
 
